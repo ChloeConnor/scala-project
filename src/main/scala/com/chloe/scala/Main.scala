b0851250bf9c5d76a7bc3c9e808d5973df613ea2
chloeconnor <chloe.connor@vortexa.com>
2020-08-03 13:51:20 +0100

    little tasks added

--- src/main/scala/com/chloe/scala/Main.scala
+++ src/main/scala/com/chloe/scala/Main.scala
@@ -2,7 +2,45 @@ package com.chloe.scala
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  def getWords(words: String): List[String] = {
+    words.split(" ").flatMap(a => a.split("\\.")).toList
+  }
+
+  def lengthOfLongestWord(words: List[String]): Int = {
+    words.map(word => word.length).max
+  }
+
+  def countSentences(fullText: String): Int = {
+    fullText.split("\\.").toList.size
+  }
+
+  def averageWordsPerSentence(fullText: String): Int = {
+    val words = getWords(fullText).size
+
+    val sentences = countSentences(fullText)
+
+    words / sentences
 
   }
+
+  def countByOccurrence(words: List[String]): List[(String, Int)] = {
+    words.groupBy(identity).mapValues(_.size).toList
+  }
+
+  def wordsOccurMost(words: List[String], howMany: Int): List[String] =
+    countByOccurrence(words)
+      .sortBy(a => a._2)(Ordering[Int].reverse)
+      .take(howMany)
+      .map(a => a._1)
+
+  def percentageOccurOnce(words: List[String]): Int = {
+    val wordCounts = countByOccurrence(words)
+    val totalWordCount: Double = words.size
+    val wordsOccurringOnce: Double = wordCounts.count(a => a._2 == 1)
+
+    ((wordsOccurringOnce / totalWordCount) * 100).toInt
+
+  }
+
+  def main(args: Array[String]): Unit = {}
 }
--- src/test/scala/com/chloe/scala/TestSomething.scala
+++ src/test/scala/com/chloe/scala/TestSomething.scala
@@ -1,10 +1,99 @@
 package com.chloe.scala
 
 import org.scalatest.flatspec.AnyFlatSpec
+import com.chloe.scala.Main.{
+  getWords,
+  lengthOfLongestWord,
+  countSentences,
+  wordsOccurMost,
+  percentageOccurOnce,
+  averageWordsPerSentence
+}
 
 class TestSomething extends AnyFlatSpec {
 
-  "Something" should "do something" in {
-    assert(1 == 1)
+  val fullText =
+    "lorem ipsum dolor sit amet consectetur lorem ipsum et mihi quoniam et adipiscing elit.sed quoniam et advesperascit et mihi ad villam revertendum est nunc quidem hactenus ex rebus enim timiditas non ex vocabulis nascitur.nummus in croesi divitiis obscuratur pars est tamen divitiarum.nam quibus rebus efficiuntur voluptates eae non sunt in potestate sapientis.hoc mihi cum tuo fratre convenit.qui ita affectus beatum esse numquam probabis duo reges constructio interrete.de hominibus dici non necesse est.eam si varietatem diceres intellegerem ut etiam non dicente te intellego parvi enim primo ortu sic iacent tamquam omnino sine animo sint.ea possunt paria non esse.quamquam tu hanc copiosiorem etiam soles dicere.de quibus cupio scire quid sentias.universa enim illorum ratione cum tota vestra confligendum puto.ut nemo dubitet eorum omnia officia quo spectare quid sequi quid fugere debeant nunc vero a primo quidem mirabiliter occulta natura est nec perspici nec cognosci potest.videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur sunt enim prima elementa naturae quibus auctis virtutis quasi germen efficitur.nam ut sint illa vendibiliora haec uberiora certe sunt.cur deinde metrodori liberos commendas.mihi inquam qui te id ipsum rogavi nam adhuc meo fortasse vitio quid ego quaeram non perspicis.quibus ego vehementer assentior.cur iustitia laudatur mihi enim satis est ipsis non satis.quid est enim aliud esse versutum nobis heracleotes ille dionysius flagitiose descivisse videtur a stoicis propter oculorum dolorem.diodorus eius auditor adiungit ad honestatem vacuitatem doloris.nos quidem virtutes sic natae sumus ut tibi serviremus aliud negotii nihil habemus."
+
+  "Words" should "be split by . or space" in {
+
+    val words = "lorem ipsum.dolor"
+
+    val expected = List("lorem", "ipsum", "dolor")
+
+    val actual = getWords(words)
+
+    assert(expected == actual)
+
   }
+
+  it should "be counted correctly" in {
+
+    val expected = 260
+
+    val actual = getWords(fullText).size
+
+    assert(expected == actual)
+  }
+
+  "Length of longest word" should "be calculated" in {
+    val words = List("lorem", "ipsum", "contemplandis")
+
+    val actualLength = lengthOfLongestWord(words)
+
+    val expected = 13
+
+    assert(expected == actualLength)
+  }
+
+  "Number of sentences" should "be counted" in {
+
+    val sentences = "lorem ipsum dolor.sit amet consectetur.lorem."
+
+    val actual = countSentences(sentences)
+    val expected = 3
+
+    assert(expected == actual)
+
+  }
+
+  "Words that occur most" should "be found" in {
+    val list = List("lorem", "ipsum", "dolor", "ipsum", "dolor", "dolor")
+
+    val expected = Set("dolor", "ipsum")
+
+    val actual = wordsOccurMost(list, 2).toSet
+
+    assert(expected == actual)
+  }
+
+  it should "be found in full text" in {
+
+    val wordsList = getWords(fullText)
+
+    val expected = Set("non", "est", "enim", "ut", "quid", "mihi")
+
+    val actual = wordsOccurMost(wordsList, 6).toSet
+
+    assert(expected == actual)
+  }
+
+  "Percentage of words occurring once" should "be calculated" in {
+
+    val wordsList = getWords(fullText)
+
+    val actual = percentageOccurOnce(wordsList)
+    val expected = 60
+
+    assert(actual == expected)
+  }
+
+  "Count sentences" should "be correct" in {
+    val actual = averageWordsPerSentence(fullText)
+    val expected = 11
+
+    assert(actual == expected)
+
+  }
+
 }
